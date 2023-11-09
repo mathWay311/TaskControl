d6204590e85bf8464defddee883abdb2be1e71ee
mathWay311 <matvej0311@gmail.com>
2023-11-10 02:38:28 +0700

    Добавлено больше всякого, всякие вьюшки, всякие модельки

---
+++ TaskControl/Models/TaskModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace TaskControl.Models
+{
+    public enum TaskStatus { Assigned, InProgress, Paused, Complete};
+    public class TaskModel
+    {
+        public int ID { get; set; }
+        public string TaskName { get; set; }
+        public string Description { get; set; }
+        public string TaskExecutors { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public TaskStatus taskStatus { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ParentID { get; set; }
+    }
+
+    public class TaskDBContext : DbContext
+    {
+         public DbSet<TaskModel> Task { get; set; }
+    }
+}
---
+++ TaskControl/Models/TaskTreeElement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskControl.Models
+{
+    public class TaskTreeElement
+    {
+        public string TaskName { get; set; }
+        public string ServiceDescription { get; set; }
+    }
+}
